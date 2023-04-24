0cd5cc624ac070421849b144ed495dfa479ad018
usamash34 <usamash34@gmail.com>
2023-04-24 22:35:17 +0100

    removed unnecessary comment after

--- JS/blogButtons.js
+++ JS/blogButtons.js
@@ -15,55 +15,6 @@ function clearButton() {
     }
 }
 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const postButton = document.getElementById("postBtn");
-
-//     postButton.addEventListener('click', function(event) {
-//         event.preventDefault();
-//         const title = document.querySelector('input[name="title"]');
-//         const message = document.querySelector('textarea[name="description"]');
-//         if (title.value.trim() === '') {
-//             title.style.borderColor = 'red';
-//         } else {
-//             title.style.borderColor = '';
-//         }
-//         if (message.value.trim() === '') {
-//             message.style.borderColor = 'red';
-//         } else {
-//             message.style.borderColor = '';
-//         }
-//         if (title.value.trim() === '' || message.value.trim() === '') {
-//             return;
-//         } else {
-//             // code to post blog (AJAX)
-
-//             const form = document.querySelector('form[name="newBlogPost"]');
-//             console.log('form:', form);
-//             if (!form) {
-//                 console.log('Form not found.');
-//                 return;
-//             }
-//             const formData = new FormData(form);
-//             const xhr = new XMLHttpRequest();
-//             xhr.open('POST', form.action);
-//             xhr.onload = function() {
-//                 if (xhr.status === 200 && xhr.responseText === 'success') {
-//                     // Do something if the request is successful
-//                     // For example, show a success message
-//                 } else {
-//                     // Do something if the request fails
-//                     // For example, show an error message
-//                 }
-//             };
-//             xhr.send(formData);
-//         }
-//     })
-// });
-
-
-
-
 function postButton(event) {
     event.preventDefault();
     const title = document.querySelector('input[name="title"]');
